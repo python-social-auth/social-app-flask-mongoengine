python-social-auth/social-storage-sqlalchemy

Cleanup python-social-auth

social_flask/routes.py
@@ -1,0 +1,45 @@
+from flask import g, Blueprint, request
+from flask_login import login_required, login_user
+
+from social.actions import do_auth, do_complete, do_disconnect
+from social_flask.utils import psa
+
+
+social_auth = Blueprint('social', __name__)
+
+
+@social_auth.route('/login/<string:backend>/', methods=('GET', 'POST'))
+@psa('social.complete')
+def auth(backend):
+    return do_auth(g.backend)
+
+
+@social_auth.route('/complete/<string:backend>/', methods=('GET', 'POST'))
+@psa('social.complete')
+def complete(backend, *args, **kwargs):
+    """Authentication complete view, override this view if transaction
+    management doesn't suit your needs."""
+    return do_complete(g.backend, login=do_login, user=g.user,
+                       *args, **kwargs)
+
+
+@social_auth.route('/disconnect/<string:backend>/', methods=('POST',))
+@social_auth.route('/disconnect/<string:backend>/<int:association_id>/',
+                   methods=('POST',))
+@social_auth.route('/disconnect/<string:backend>/<string:association_id>/',
+                   methods=('POST',))
+@login_required
+@psa()
+def disconnect(backend, association_id=None):
+    """Disconnects given backend from current logged in user."""
+    return do_disconnect(g.backend, g.user, association_id)
+
+
+def do_login(backend, user, social_user):
+    name = backend.strategy.setting('REMEMBER_SESSION_NAME', 'keep')
+    remember = backend.strategy.session_get(name) or \
+               request.cookies.get(name) or \
+               request.args.get(name) or \
+               request.form.get(name) or \
+               False
+    return login_user(user, remember=remember)

social_flask/strategy.py
@@ -1,0 +1,56 @@
+from flask import current_app, request, redirect, make_response, session, \
+                  render_template, render_template_string
+
+from social.utils import build_absolute_uri
+from social.strategy import BaseStrategy, BaseTemplateStrategy
+
+
+class FlaskTemplateStrategy(BaseTemplateStrategy):
+    def render_template(self, tpl, context):
+        return render_template(tpl, **context)
+
+    def render_string(self, html, context):
+        return render_template_string(html, **context)
+
+
+class FlaskStrategy(BaseStrategy):
+    DEFAULT_TEMPLATE_STRATEGY = FlaskTemplateStrategy
+
+    def get_setting(self, name):
+        return current_app.config[name]
+
+    def request_data(self, merge=True):
+        if merge:
+            data = request.form.copy()
+            data.update(request.args)
+        elif request.method == 'POST':
+            data = request.form
+        else:
+            data = request.args
+        return data
+
+    def request_host(self):
+        return request.host
+
+    def redirect(self, url):
+        return redirect(url)
+
+    def html(self, content):
+        response = make_response(content)
+        response.headers['Content-Type'] = 'text/html;charset=UTF-8'
+        return response
+
+    def session_get(self, name, default=None):
+        return session.get(name, default)
+
+    def session_set(self, name, value):
+        session[name] = value
+
+    def session_pop(self, name):
+        return session.pop(name, None)
+
+    def session_setdefault(self, name, value):
+        return session.setdefault(name, value)
+
+    def build_absolute_uri(self, path=None):
+        return build_absolute_uri(request.host_url, path)

social_flask/template_filters.py
@@ -1,0 +1,25 @@
+from flask import g, request
+
+from social.backends.utils import user_backends_data
+from social_flask.utils import get_helper
+
+
+def backends():
+    """Load Social Auth current user data to context under the key 'backends'.
+    Will return the output of social.backends.utils.user_backends_data."""
+    return {
+        'backends': user_backends_data(g.user,
+                                       get_helper('AUTHENTICATION_BACKENDS'),
+                                       get_helper('STORAGE', do_import=True))
+    }
+
+
+def login_redirect():
+    """Load current redirect to context."""
+    value = request.form.get('next', '') or \
+            request.args.get('next', '')
+    return {
+        'REDIRECT_FIELD_NAME': 'next',
+        'REDIRECT_FIELD_VALUE': value,
+        'REDIRECT_QUERYSTRING': value and ('next=' + value) or ''
+    }

python-social-auth/social-app-flask-mongoengine

social_flask/routes.py
@@ -1,0 +1,45 @@
+from flask import g, Blueprint, request
+from flask_login import login_required, login_user
+
+from social_core.actions import do_auth, do_complete, do_disconnect
+from social_flask.utils import psa
+
+
+social_auth = Blueprint('social', __name__)
+
+
+@social_auth.route('/login/<string:backend>/', methods=('GET', 'POST'))
+@psa('social.complete')
+def auth(backend):
+    return do_auth(g.backend)
+
+
+@social_auth.route('/complete/<string:backend>/', methods=('GET', 'POST'))
+@psa('social.complete')
+def complete(backend, *args, **kwargs):
+    """Authentication complete view, override this view if transaction
+    management doesn't suit your needs."""
+    return do_complete(g.backend, login=do_login, user=g.user,
+                       *args, **kwargs)
+
+
+@social_auth.route('/disconnect/<string:backend>/', methods=('POST',))
+@social_auth.route('/disconnect/<string:backend>/<int:association_id>/',
+                   methods=('POST',))
+@social_auth.route('/disconnect/<string:backend>/<string:association_id>/',
+                   methods=('POST',))
+@login_required
+@psa()
+def disconnect(backend, association_id=None):
+    """Disconnects given backend from current logged in user."""
+    return do_disconnect(g.backend, g.user, association_id)
+
+
+def do_login(backend, user, social_user):
+    name = backend.strategy.setting('REMEMBER_SESSION_NAME', 'keep')
+    remember = backend.strategy.session_get(name) or \
+               request.cookies.get(name) or \
+               request.args.get(name) or \
+               request.form.get(name) or \
+               False
+    return login_user(user, remember=remember)

social_flask/strategy.py
@@ -1,0 +1,56 @@
+from flask import current_app, request, redirect, make_response, session, \
+                  render_template, render_template_string
+
+from social_core.utils import build_absolute_uri
+from social_core.strategy import BaseStrategy, BaseTemplateStrategy
+
+
+class FlaskTemplateStrategy(BaseTemplateStrategy):
+    def render_template(self, tpl, context):
+        return render_template(tpl, **context)
+
+    def render_string(self, html, context):
+        return render_template_string(html, **context)
+
+
+class FlaskStrategy(BaseStrategy):
+    DEFAULT_TEMPLATE_STRATEGY = FlaskTemplateStrategy
+
+    def get_setting(self, name):
+        return current_app.config[name]
+
+    def request_data(self, merge=True):
+        if merge:
+            data = request.form.copy()
+            data.update(request.args)
+        elif request.method == 'POST':
+            data = request.form
+        else:
+            data = request.args
+        return data
+
+    def request_host(self):
+        return request.host
+
+    def redirect(self, url):
+        return redirect(url)
+
+    def html(self, content):
+        response = make_response(content)
+        response.headers['Content-Type'] = 'text/html;charset=UTF-8'
+        return response
+
+    def session_get(self, name, default=None):
+        return session.get(name, default)
+
+    def session_set(self, name, value):
+        session[name] = value
+
+    def session_pop(self, name):
+        return session.pop(name, None)
+
+    def session_setdefault(self, name, value):
+        return session.setdefault(name, value)
+
+    def build_absolute_uri(self, path=None):
+        return build_absolute_uri(request.host_url, path)

social_flask/template_filters.py
@@ -1,0 +1,25 @@
+from flask import g, request
+
+from social_core.backends.utils import user_backends_data
+from social_flask.utils import get_helper
+
+
+def backends():
+    """Load Social Auth current user data to context under the key 'backends'.
+    Will return the output of social.backends.utils.user_backends_data."""
+    return {
+        'backends': user_backends_data(g.user,
+                                       get_helper('AUTHENTICATION_BACKENDS'),
+                                       get_helper('STORAGE', do_import=True))
+    }
+
+
+def login_redirect():
+    """Load current redirect to context."""
+    value = request.form.get('next', '') or \
+            request.args.get('next', '')
+    return {
+        'REDIRECT_FIELD_NAME': 'next',
+        'REDIRECT_FIELD_VALUE': value,
+        'REDIRECT_QUERYSTRING': value and ('next=' + value) or ''
+    }